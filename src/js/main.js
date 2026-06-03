bee71d50b7fb242b3dc87e8e1ba8a4396d6ce7aa
elysianlin <lin24024@byui.edu>
2026-06-03 16:40:50 +0800

    w07

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
-import { getParkData, getInfoLinks, getParkAlerts } from "./parkService.mjs";
+import "../css/style.css"; // we can do this because we are using Vite...
+import "../css/home.css";
+import { getParkData, getInfoLinks } from "./parkService.mjs";
 import setHeaderFooter from "./setHeaderFooter.mjs";
 import { mediaCardTemplate } from "./templates.mjs";
 
@@ -10,19 +12,43 @@ function setParkIntro(data) {
 
 function setParkInfoLinks(data) {
   const infoEl = document.querySelector(".info");
+  // we have multiple links to build...so we map to transform the array of objects into an array of HTML strings.
   const html = data.map(mediaCardTemplate);
+  // join the array of strings into one string and insert it into the section
   infoEl.insertAdjacentHTML("afterbegin", html.join(""));
 }
-
 
 async function init() {
   const parkData = await getParkData();
   const links = getInfoLinks(parkData.images);
-  const alerts = await getParkAlerts(parkData.parkCode);
   setHeaderFooter(parkData);
   setParkIntro(parkData);
   setParkInfoLinks(links);
-  setAlerts(alerts);
+}
+
+function enableNavigation() {
+  const menuButton = document.querySelector("#global-nav-toggle");
+  // when the main menu button is clicked:
+  menuButton.addEventListener("click", (ev) => {
+    let target = ev.target;
+    // toggle the show class on the global-nav
+    document.querySelector(".global-nav").classList.toggle("show");
+    // check to see if ev.target is the button or something inside the button
+    if (target.tagName != "BUTTON") {
+      target = target.closest("button");
+    }
+    // check to see if we just opened or closed the menu
+    if (document.querySelector(".global-nav").classList.contains("show")) {
+      // if we opened it then set the aria-expanded attribute to true
+      target.setAttribute("aria-expanded", true);
+    } else {
+      // if we closed it then set the aria-expanded attribute to false
+      target.setAttribute("aria-expanded", false);
+    }
+
+    console.log("toggle");
+  });
 }
 
 init();
+enableNavigation();
